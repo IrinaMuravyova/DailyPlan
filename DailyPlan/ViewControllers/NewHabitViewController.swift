8ddf552b11d2ea01e4696ecf537531d13577ed7f
IrinaMuravyova <miomir@yandex.ru>
2024-10-31 14:43:41 +0800

    compeleted adding of new habit and show it on main screen

---
+++ DailyPlan/ViewControllers/NewHabitViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewHabitViewController.swift
+//  DailyPlan
+//
+//  Created by Irina Muravyeva on 31.10.2024.
+//
+
+import UIKit
+
+class NewHabitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
